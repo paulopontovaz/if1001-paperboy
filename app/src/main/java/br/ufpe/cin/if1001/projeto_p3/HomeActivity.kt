f30664575a157359114b76a5101e70f055da2370
Paulo Vaz <paulopontovaz@gmail.com>
2018-06-03 19:02:38 -0300

    Criando layouts e activities

--- app/src/main/java/br/ufpe/cin/if1001/projeto_p3/HomeActivity.kt
+++ app/src/main/java/br/ufpe/cin/if1001/projeto_p3/HomeActivity.kt
@@ -3,10 +3,10 @@ package br.ufpe.cin.if1001.projeto_p3
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
