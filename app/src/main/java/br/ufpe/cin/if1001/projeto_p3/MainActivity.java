7934a2b26caeece25c956a1b1db582319c145e46
vapb7 <vapb@cin.ufpe.br>
2018-06-05 21:37:03 -0300

    implementando o menu lateral

--- app/src/main/java/br/ufpe/cin/if1001/projeto_p3/MainActivity.java
+++ app/src/main/java/br/ufpe/cin/if1001/projeto_p3/MainActivity.java
@@ -1,13 +1,69 @@
 package br.ufpe.cin.if1001.projeto_p3;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.internal.NavigationMenu;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle =  new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_id);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.favoritos_menu){
+            Intent favoritosIntent = new Intent(getApplicationContext(), FavoritesActivity.class);
+            startActivity(favoritosIntent);
+        }
+        if (id == R.id.home_menu){
+            Intent homeIntent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(homeIntent);
+        }
+        if (id == R.id.inscricoes_menu){
+            Intent inscricoesIntent = new Intent(getApplicationContext(), FeedsActivity.class);
+            startActivity(inscricoesIntent);
+        }
+        if (id == R.id.lermaistarde_menu){
+            Intent lermaistardeIntent = new Intent(getApplicationContext(), ViewLaterActivity.class);
+            startActivity(lermaistardeIntent);
+        }
+        if (id == R.id.teste_menu){
+            Intent testeIntent = new Intent(getApplicationContext(), ReaderActivity.class);
+            startActivity(testeIntent);
+        }
+
+        return false;
     }
 }
+
